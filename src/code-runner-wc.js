baa225c58d0145357ed4a9e0b16e9937cc0369d0
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-27 06:10:47 -0400

    Progress on automatic version detection :wrench:

--- src/code-runner-wc.js
+++ src/code-runner-wc.js
@@ -1,3 +1,10 @@
+// All the supported languages / aliases for language names + versions (used for auto-detecting version to use) 
+let PistonAPI_Languages = [{"language":"matl","version":"22.5.0","aliases":[]},{"language":"bash","version":"5.1.0","aliases":["sh"]},{"language":"befunge93","version":"0.2.0","aliases":["b93"]},{"language":"brachylog","version":"1.0.0","aliases":[]},{"language":"brainfuck","version":"2.7.3","aliases":["bf"]},{"language":"cjam","version":"0.6.5","aliases":[]},{"language":"clojure","version":"1.10.3","aliases":["clojure","clj"]},{"language":"cobol","version":"3.1.2","aliases":["cob"]},{"language":"coffeescript","version":"2.5.1","aliases":["coffeescript","coffee"]},{"language":"cow","version":"1.0.0","aliases":["cow"]},{"language":"crystal","version":"0.36.1","aliases":["crystal","cr"]},{"language":"dart","version":"2.12.1","aliases":[]},{"language":"dash","version":"0.5.11","aliases":["dash"]},{"language":"typescript","version":"1.16.2","aliases":["deno","deno-ts"],"runtime":"deno"},{"language":"javascript","version":"1.16.2","aliases":["deno-js"],"runtime":"deno"},{"language":"basic.net","version":"5.0.201","aliases":["basic","visual-basic","visual-basic.net","vb","vb.net","vb-dotnet","dotnet-vb","basic-dotnet","dotnet-basic"],"runtime":"dotnet"},{"language":"fsharp.net","version":"5.0.201","aliases":["fsharp","fs","f#","fs.net","f#.net","fsharp-dotnet","fs-dotnet","f#-dotnet","dotnet-fsharp","dotnet-fs","dotnet-fs"],"runtime":"dotnet"},{"language":"csharp.net","version":"5.0.201","aliases":["csharp","c#","cs","c#.net","cs.net","c#-dotnet","cs-dotnet","csharp-dotnet","dotnet-c#","dotnet-cs","dotnet-csharp"],"runtime":"dotnet"},{"language":"fsi","version":"5.0.201","aliases":["fsx","fsharp-interactive","f#-interactive","dotnet-fsi","fsi-dotnet","fsi.net"],"runtime":"dotnet"},{"language":"dragon","version":"1.9.8","aliases":[]},{"language":"elixir","version":"1.11.3","aliases":["elixir","exs"]},{"language":"emacs","version":"27.1.0","aliases":["emacs","el","elisp"]},{"language":"emojicode","version":"1.0.2","aliases":["emojic"]},{"language":"erlang","version":"23.0.0","aliases":["erlang","erl","escript"]},{"language":"file","version":"0.0.1","aliases":["executable","elf","binary"]},{"language":"forte","version":"1.0.0","aliases":["forter"]},{"language":"forth","version":"0.7.3","aliases":["gforth"]},{"language":"freebasic","version":"1.8.0","aliases":["bas","fbc","basic","qbasic","quickbasic"]},{"language":"awk","version":"5.1.0","aliases":["gawk"],"runtime":"gawk"},{"language":"c","version":"10.2.0","aliases":["gcc"],"runtime":"gcc"},{"language":"c++","version":"10.2.0","aliases":["cpp","g++"],"runtime":"gcc"},{"language":"d","version":"10.2.0","aliases":["gdc"],"runtime":"gcc"},{"language":"fortran","version":"10.2.0","aliases":["fortran","f90"],"runtime":"gcc"},{"language":"go","version":"1.16.2","aliases":["go","golang"]},{"language":"golfscript","version":"1.0.0","aliases":["golfscript"]},{"language":"groovy","version":"3.0.7","aliases":["groovy","gvy"]},{"language":"haskell","version":"9.0.1","aliases":["haskell","hs"]},{"language":"husk","version":"1.0.0","aliases":[]},{"language":"iverilog","version":"11.0.0","aliases":["verilog","vvp"]},{"language":"japt","version":"2.0.0","aliases":["japt"]},{"language":"java","version":"15.0.2","aliases":[]},{"language":"jelly","version":"0.1.31","aliases":[]},{"language":"julia","version":"1.6.1","aliases":["jl"]},{"language":"kotlin","version":"1.4.31","aliases":["kt"]},{"language":"lisp","version":"2.1.2","aliases":["lisp","cl","sbcl","commonlisp"]},{"language":"llvm_ir","version":"12.0.1","aliases":["llvm","llvm-ir","ll"]},{"language":"lolcode","version":"0.11.2","aliases":["lol","lci"]},{"language":"lua","version":"5.4.2","aliases":["lua"]},{"language":"csharp","version":"6.12.0","aliases":["mono","mono-csharp","mono-c#","mono-cs","c#","cs"],"runtime":"mono"},{"language":"basic","version":"6.12.0","aliases":["vb","mono-vb","mono-basic","visual-basic","visual basic"],"runtime":"mono"},{"language":"nasm","version":"2.15.5","aliases":["asm","nasm32"],"runtime":"nasm"},{"language":"nasm64","version":"2.15.5","aliases":["asm64"],"runtime":"nasm"},{"language":"nim","version":"1.4.4","aliases":["nim"]},{"language":"javascript","version":"16.3.0","aliases":["node-javascript","node-js","javascript","js"],"runtime":"node"},{"language":"ocaml","version":"4.12.0","aliases":["ocaml","ml"]},{"language":"octave","version":"6.2.0","aliases":["matlab","m"]},{"language":"osabie","version":"1.0.1","aliases":["osabie","05AB1E","usable"]},{"language":"paradoc","version":"0.6.0","aliases":["paradoc"]},{"language":"pascal","version":"3.2.0","aliases":["pascal","freepascal","pp","pas"]},{"language":"perl","version":"5.26.1","aliases":["perl","pl"]},{"language":"php","version":"8.0.2","aliases":["php8","html"]},{"language":"ponylang","version":"0.39.0","aliases":["pony","ponyc"]},{"language":"prolog","version":"8.2.4","aliases":["prolog","plg"]},{"language":"pure","version":"0.68.0","aliases":[]},{"language":"powershell","version":"7.1.4","aliases":["ps","pwsh","ps1"],"runtime":"pwsh"},{"language":"pyth","version":"1.0.0","aliases":["pyth"]},{"language":"python2","version":"2.7.18","aliases":["py2","python2"]},{"language":"python","version":"3.10.0","aliases":["py","py3","python3","python3.10"]},{"language":"racket","version":"8.3.0","aliases":["rkt"]},{"language":"raku","version":"6.100.0","aliases":["raku","rakudo","perl6","p6","pl6"]},{"language":"retina","version":"1.2.0","aliases":["ret"]},{"language":"rockstar","version":"1.0.0","aliases":["rock","rocky"]},{"language":"rscript","version":"4.1.1","aliases":["r"]},{"language":"ruby","version":"3.0.1","aliases":["ruby3","rb"]},{"language":"rust","version":"1.56.1","aliases":["rs"]},{"language":"scala","version":"3.0.0","aliases":["sc"]},{"language":"smalltalk","version":"3.2.3","aliases":["st"]},{"language":"sqlite3","version":"3.36.0","aliases":["sqlite","sql"]},{"language":"swift","version":"5.3.3","aliases":["swift"]},{"language":"typescript","version":"4.2.3","aliases":["ts","node-ts","tsc"]},{"language":"vlang","version":"0.1.13","aliases":["v"]},{"language":"vyxal","version":"2.4.1","aliases":[]},{"language":"yeethon","version":"3.10.0","aliases":["yeethon3"]},{"language":"zig","version":"0.8.0","aliases":["zig"]}]
+
+
+
+
+
 class CodeRunner extends HTMLElement {
 	constructor() {
 		super();
@@ -9,7 +16,7 @@ class CodeRunner extends HTMLElement {
 		// will be used to set language type to use and names of languages 
 		const language = this.getAttribute('language');
 
-		const version = this.getAttribute('version');
+	
 
 		const styling = `<style>
     
@@ -161,12 +168,12 @@ class CodeRunner extends HTMLElement {
  </style>
  `
 
-		if (!language || !version) {
+		if (!language) {
 			return this.innerHTML = `${styling}   <!--compress-->  <div>
   
 <pre style="padding: 0px; --bg:rgb(58, 54, 54); --text:rgb(255, 255, 255); --border:rgba(0,0,0,0.3); --code:rgb(255, 255, 255); --code-bg:rgba(39, 40, 35, 1); --title:rgb(255, 255, 255); --button-text:wheat; --button-border:rgba(0,0,0,0.18);"><div class="code-knack-playground "><div class="code-knack-pane"><div class="code-knack-title">Error!</div>
   
-</div><div id="codetorun" class="code-knack-text" style="/* display: none; */">Error: Required HTML Attributes Missing For Web Component ...
+</div><div id="codetorun" class="code-knack-text" style="/* display: none; */">Error: Language Attribute Is Missing For Web Component ...
         </div>
         
       </div>
@@ -182,7 +189,7 @@ class CodeRunner extends HTMLElement {
     
     <!--compress-->
     
-    <div code-runner-component language=${language} version=${version}>
+    <div code-runner-component language=${language}>
   
 <pre style="padding: 0px; --bg:rgb(58, 54, 54); --text:rgb(255, 255, 255); --border:rgba(0,0,0,0.3); --code:rgb(255, 255, 255); --code-bg:rgba(39, 40, 35, 1); --title:rgb(255, 255, 255); --button-text:wheat; --button-border:rgba(0,0,0,0.18);"><div class="code-knack-playground " data-lang="cpp" data-options=""><div class="code-knack-pane"><div class="code-knack-title">${language}</div>
   <div class="code-knack-mask">Copied to the clipboard.</div>
@@ -222,7 +229,7 @@ async function getData(html_element) {
 			method: 'POST',
 			body: JSON.stringify({
 				"language": html_element.getAttribute("language").toLowerCase(),
-				"version": html_element.getAttribute("version"),
+				"version": GetVersionForPistonAPI(html_element.getAttribute("language").toLowerCase()),
 				"files": [{
 					"content": html_element.querySelector("#codetorun").innerText
 				}]
@@ -274,4 +281,37 @@ function handleCopyBtnClick(html_element) {
    html_element.querySelector(".code-knack-mask").style.display = "none";
 }, "3000")
 
+}
+/// Provides the version to use for Piston API automatically! 
+function GetVersionForPistonAPI(string){
+data = PistonAPI_Languages  
+for (const key in data){
+  if (!string == data[key].language){
+    console.log(`Error: Code-Runner could not auto-detect version to use for ${string}`)
+    return "undefined"
+  
+  }
+  
+ if (string == data[key].language){
+      console.log(`found ${data[key].version} for ${string}` )
+    return data[key].version
+ } else {
+   for (const aliasName in data[key].aliases){
+    if (string == data[key].aliases[aliasName]){
+      console.log(`found ${data[key].version} for ${string}` )
+         return data[key].version
+      
+    }
+      // Need to fix this and throw error
+     //  else{
+     // console.log(`Error: Code-Runner could not auto-detect version to use for ${string}`)
+   // return "undefined"
+   // }
+     
+  
+   }
+     
+ }
+  
+}
 }
