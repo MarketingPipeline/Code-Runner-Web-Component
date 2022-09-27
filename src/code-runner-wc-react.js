3851cd4559e921677fd566fab1fb383684250bf5
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-09-27 05:43:45 -0400

    Removed un-needed file :wastebasket:

--- src/code-runner-wc-react.js
+++
@@ -1,9 +0,0 @@
-import "https://cdn.jsdelivr.net/gh/MarketingPipeline/Code-Runner-Web-Component/dist/code-runner-wc.min.js";
-
-import useCustomElement from "https://cdn.skypack.dev/use-custom-element@1.0.5";
-
-const CodeRunnerComponent = props => {
-  const [customElementProps, ref] = useCustomElement(props);
-
-  return <code-runner {...customElementProps} ref={ref} />;
-};
