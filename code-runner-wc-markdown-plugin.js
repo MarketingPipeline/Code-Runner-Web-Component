a2c9b6368017619d7e6cdd8c86147b24bba8edf5
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-11-12 18:04:39 -0500

    Create code-runner-wc-markdown-plugin.js

---
+++ code-runner-wc-markdown-plugin.js
@@ -0,0 +1,84 @@
+
+/// Handle all button clicks for WC
+
+const web_compontents = document.querySelectorAll('[code-runner-component]');
+
+web_compontents.forEach((web_compontent) => web_compontent.querySelector('[ code-runner-button]').addEventListener('click', (e) => handleClick(web_compontent)));
+
+function handleClick(button) {
+if (button.getAttribute("language").toLowerCase() === "latex" || "markdown" || "md"){
+      if (button.getAttribute("language") === "latex"){
+        renderLatex(button)
+      }
+  
+        if (button.getAttribute("language") === "markdown" || "md"){
+       renderMarkdown(button)
+      }
+  
+  
+    }
+
+}
+
+
+
+
+
+function renderMarkdown(html_element){
+ 
+  let converter = new showdown.Converter({
+    tables: true,
+    simpleLineBreaks: true
+  })
+      console.log(html_element.querySelector("#codetorun").innerHTML)
+  	const result_section = html_element.querySelector("#output_section")
+	result_section.style.display = "block";
+html_element.querySelector("#output_section").innerHTML = converter.makeHtml(html_element.querySelector("#codetorun").innerText)
+  
+}
+
+
+
+document.querySelectorAll("code-runner").forEach((el) => {
+  if (el.getAttribute("language").toLowerCase() === "latex") {
+  loadJavaScriptFile('https://cdn.jsdelivr.net/npm/katex@0.10.0-rc.1/dist/katex.js')
+  }
+  
+    if (el.getAttribute("language").toLowerCase() === "md" || "markdown") {
+  loadJavaScriptFile('https://cdnjs.cloudflare.com/ajax/libs/showdown/1.8.7/showdown.min.js')
+  }
+    
+
+})
+
+
+
+function loadJavaScriptFile(url){
+  
+    /// Add Markdown Parser To Document
+var script = document.createElement('script');
+script.src = url;
+
+document.head.appendChild(script); //or something of the likes  
+  
+  
+  
+  
+  // On Error Loading Markdown Parser
+script.onerror = function () {
+ 
+  console.error("Markdown Tag: Error while performing function LoadMarkdownParser - There was an error loading the Markdown Parser")
+  
+}
+
+  
+  /// Markdown Parser Load Successful 
+script.onload = function () {
+ 
+  // Append this CSS
+
+// https://cdn.jsdelivr.net/npm/katex@0.10.0-rc.1/dist/katex.css 
+  
+};
+}
+
