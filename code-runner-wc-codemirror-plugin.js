607244452c27c7907308ff05154125239bb896f0
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-11-12 19:08:26 -0500

    Create code-runner-wc-codemirror-plugin.js

---
+++ code-runner-wc-codemirror-plugin.js
@@ -0,0 +1,27 @@
+
+
+const web_compontents = document.querySelectorAll('[code-runner-component]');
+
+web_compontents.forEach((web_compontent) => addCodeMirror(web_compontent));
+
+function addCodeMirror(button) {
+	
+	
+// Remove the content editable version editor
+	// turn into text area
+button.querySelector('[contenteditable]').outerHTML = button.querySelector('[contenteditable]').outerHTML.replaceAll('div', 'textarea')
+	
+	// remove content editable attribute
+	button.querySelector("#codetorun").removeAttribute("contenteditable");
+
+	
+	// iniate the editor. 
+let editor = CodeMirror.fromTextArea(button.querySelector("#codetorun"), {
+    mode: "python",
+    lineNumbers: true,
+});
+  
+editor.save()
+
+}
+
