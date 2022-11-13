c1e22d62ef8e4aeced886acfb4f0ca5c408fd94c
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-11-13 04:17:10 -0500

    Create phpuniter-terminal.js

---
+++ phpuniter-terminal.js
@@ -0,0 +1,45 @@
+window.addEventListener('load', (event) => {
+    
+const web_compontents = document.querySelectorAll('[code-runner-component]');
+
+web_compontents.forEach((web_compontent) => t(web_compontent));    
+
+ function t(b){   
+     
+     // store pre-defined code to run
+   let preDefinedCode =  b.querySelector("#codetorun").innerHTML    
+   // clear text from html 
+   b.querySelector("#codetorun").innerHTML = ""
+   
+var term = $(b.querySelector("#codetorun")).terminal(function(command) {
+    console.log(command)
+    php.execute('<?php ' + command);
+}, {
+    greetings: 'Interactive PHP shell\n',
+    name: 'php',
+    prompt: 'php > ' // same as linux exec
+});
+
+// PHP to string magic method
+$.fn.__toString = function() {
+    return `#<jQuery(${this.length})>`;
+};
+
+$.terminal.syntax('php')
+
+var php = uniter.createEngine('PHP');
+php.getStdout().on('data', function (text) {
+    term.echo(text);
+});
+php.getStderr().on('data', function(text) {
+    term.error(text);
+});
+
+php.expose(term, 'term');
+php.expose($, 'jQuery');
+ php.execute('<?php ' +  preDefinedCode);
+     console.log( preDefinedCode)   
+   }
+                     
+                 
+});
